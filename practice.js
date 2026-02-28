17f7eedc79e91571fb716121103a80e13bbdf969
shuvo <shuvovlog223@gmail.com>
2026-02-28 15:23:22 +0600

    Initial clean commit

---
+++ practice.js
@@ -0,0 +1,84 @@
+//practice - 1, page no: 47
+let income = 100;
+let spend = 40;
+
+let Money = income - spend;
+console.log(Money);
+
+//practice 2
+let penCil = 10; //i have 10 pencil
+let usePer = 2; //everytime i use 2 pencile,
+
+let canUse = penCil / usePer; //How many time i can use these pencils?
+console.log(canUse);
+
+//practice 3
+
+let totalMoney = 60; // total money i have
+let spendPer = 15; //i can spend 15 tk each time
+
+let howManyTimes = totalMoney / spendPer; //how many times i can spend?
+console.log(howManyTimes);
+
+//practice 4
+
+let wordOne = 'Hello';
+let wordTwo = 'World';
+
+console.log(wordOne + wordTwo);
+
+//practice 5
+
+let chalKg = 153;
+let dalKg = 261;
+
+let totalKg = chalKg + dalKg;
+console.log(totalKg);
+
+//practice 6
+
+let moneyHave = 500; // total money
+let perSpend = 75; //i can spend 75 tk per time
+
+let canSpendTime = moneyHave / perSpend;
+console.log(canSpendTime); // 6.666 times i can spend 75 tk
+
+let totalSpend = 6 * perSpend; // i just take purno songkha, < beacuse i can not use 0.66>
+let restMoney = moneyHave - totalSpend;
+console.log(restMoney); // total money i have after spend.
+
+//practice 7
+
+const firstNumber = 8;
+const secondNumber = 3;
+
+const vagFol = firstNumber / secondNumber; // solved vagfol
+
+const vagSes = firstNumber % secondNumber; // solved vagses
+console.log(vagFol, vagSes);
+
+//practice 8
+
+const numberOne = 50;
+const numberTwo = 9;
+
+const bagFol = numberOne % numberTwo;
+console.log(bagFol);
+
+//practice 9
+
+const firstWord = 'Bangla';
+const secondWord = 'desh';
+
+console.log(firstWord + secondWord);
+
+//practice 10
+
+const perDay = 4; // 4kg rice
+const iHave = 480; // i have total 480kg rice or 12 mon
+
+const dinJabe = iHave / perDay;
+console.log(dinJabe); // output = 120 days
+
+const monthJabe = dinJabe / 30; // becuse 1 month = 30 days. output = 4 month
+console.log(monthJabe);
