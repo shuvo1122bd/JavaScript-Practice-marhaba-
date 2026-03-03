00002d68e0b1408aeda74d9eca74b058b0aea82b
shuvo <shuvovlog223@gmail.com>
2026-03-04 04:56:50 +0600

    new files and problem added

---
+++ array.js
@@ -0,0 +1,70 @@
+//Practice Array
+//1
+const number = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+const fruits = ['apple', 'banana', 'orange', 'mango', 'jack-fruit'];
+
+console.log(number.length);
+//2
+const bazar = [
+  'potato',
+  'onion',
+  'begun',
+  'mula',
+  'gajor',
+  'tomato',
+  'derosh',
+  'lauu',
+  'sosha',
+  'potol',
+];
+
+console.log(bazar.length);
+//3
+const favMovie = [
+  'lord of the ring',
+  'pirates of the caribbean',
+  'salaar',
+  'osman',
+  'ertugrul',
+]; // favourite number array
+//4
+const oddNumber = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29]; //odd number array
+//5
+const color = [
+  'yellow',
+  'pink',
+  'red',
+  'black',
+  'aqua',
+  'aquamarine',
+  'white',
+  'blue',
+]; //color array
+
+console.log(color.length);
+//6
+const capitalCity = [
+  'dhaka',
+  'islamabad',
+  'tokyo',
+  'katmondu',
+  'riyad',
+  'dubai',
+];
+console.log(capitalCity.length);
+
+//index, position array
+//1
+const friendsName = ['rahim', 'karim', 'muhim', 'iqbal', 'shofiq'];
+console.log(friendsName[3]);
+
+//2
+const booksName = ['novel', 'hovel', 'govel', 'mavel', 'lovel', 'ovel'];
+let serialNumber = booksName[5];
+console.log(serialNumber);
+
+//3
+const numberTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numberTwo[7] = 10;
+console.log(numberTwo);
+
