8f14f45de75200843c34d7ac2811aa68dd7f7222
shuvo <shuvovlog223@gmail.com>
2026-03-06 03:14:47 +0600

    added loop practices

--- array.js
+++ array.js
@@ -65,6 +65,104 @@ console.log(serialNumber);
 
 //3
 const numberTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-numberTwo[7] = 10;
+numberTwo[7] = 30;
 console.log(numberTwo);
 
+//4
+const favGame = ['freeFire', 'pubg', 'candy crush', 'temple run'];
+favGame[2] = 'subway surfer';
+console.log(favGame);
+
+//5
+const familyMembers = ['shuvo', 'shahed', 'shumi', 'bodrul', 'rehana'];
+console.log(familyMembers[3]);
+
+//6
+const travelCountry = [
+  'saudi arabia',
+  'switzerland',
+  'japan',
+  'spain',
+  'england',
+  'canada',
+  'palestine',
+];
+
+let check4thCountry = travelCountry[4];
+console.log(check4thCountry);
+
+//7
+const itemOnTable = ['tape', 'speaker', 'usb port', 'notepad'];
+
+console.log(itemOnTable[7]);
+
+//push(), pop(), shift(), unshift()
+
+//1
+const newNumbers = [10, 20, 30, 40, 50];
+newNumbers.push(60);
+console.log(newNumbers);
+
+//2
+const newFriends = ['sojib', 'sagor', 'sakib', 'sohel'];
+newFriends.push('sumon');
+console.log(newFriends);
+
+//3
+const mobileGame = ['call of duty', 'eFootball', 'clash of clans', 'warfare'];
+mobileGame.pop();
+console.log(mobileGame);
+
+//4
+const someNumbers = [25, 36, 48, 60];
+someNumbers.unshift(12);
+console.log(someNumbers);
+
+//5
+const tBooks = [
+  'habluder programming',
+  'javascript book',
+  'abul book',
+  'tabul book',
+  'antaji book',
+];
+tBooks.shift();
+console.log(tBooks);
+
+//1 page 102
+const fruitsHave = ['apple', 'banana', 'mango', 'litchi'];
+
+if (fruitsHave.includes('mango')) {
+  console.log('aaam ache');
+} else {
+  console.log('aam nei, gaache utt');
+}
+
+//2
+const nameAbul = ['babul', 'alif', 'chuton'];
+const index = nameAbul.indexOf('babul');
+console.log(index);
+
+//3
+const nameFrabul = ['rimon', 'rifat', 'rajib'];
+const indeex = nameFrabul.indexOf('rifat');
+console.log(indeex);
+
+//4
+const cityOfbd = ['Dhaka', 'Chittagong', 'Sylhet'];
+cityOfbd.push('rajshahi');
+const inCludes = cityOfbd.includes('Rajshahi');
+console.log(inCludes);
+
+//5
+const season = ['dighi', 'meg', 'brishti', 'borsha'];
+
+if (season.includes('brishti')) {
+  console.log("i need umbrella");
+} else {
+  console.log('No rain no pain');
+}
+
+//6
+const favGamePlay = ['cricket', 'football', 'vollyball'];
+console.log(favGamePlay.includes("badminton"));
---
+++ loop.js
@@ -0,0 +1,77 @@
+//1 page 105
+let subject = ['english', 'politics', 'economics', 'statistics', 'biography'];
+
+for (let count of subject){
+  console.log(count);
+}
+
+//2
+const favFood = ['pulao', 'kacchi', 'chicken', 'fried fish', 'chicken-fry'];
+
+for (const printAll of favFood) {
+  console.log(printAll);
+}
+
+//3
+const birthDate = [1999, 2000, 2007,];
+
+for (const printBirth of birthDate) {
+  console.log(printBirth);
+}
+
+//4
+const favPlayer = ['Messi', 'ronaldo', 'neymar', 'ozil', 'mohammad salah', 'kavin de bruyne', 'goals count', 450];
+
+for (const printPlayer of favPlayer) {
+  console.log(printPlayer);
+}
+
+const examDate = ['Bangla = 15 march', "English = 17 march", "math = 20 march"];
+
+for (const printExamDate of examDate) {
+  console.log(printExamDate);
+}
+
+// while-loop
+//1
+let num = 0;
+
+while (num < 10) {
+  console.log('i will practice programming 3h daily');
+  num = num + 1;
+}
+
+//2
+let num2 = 1;
+
+while (num2 <= 20) {
+  console.log(num2);
+  num2++;
+}
+
+//3
+let num3 = 50;
+
+while (num3 <= 100) {
+  console.log(num3);
+  num3++;
+}
+
+// //4
+// let num4 = 1;
+
+// while (num4 <= 10) {
+//   console.log(num4);
+//   num4 = num4 * 3;
+//   num++;
+// }
+
+//5 
+let num5 = 10;
+
+while (num5 < 110) {
+  console.log(num5);
+  num5 = num5 * 2;
+  
+}
+
